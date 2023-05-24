66548b3c5e58b4587984845f1fc50af1f8721f2d
whiteplusyellow <elaine.agustina0501@gmail.com>
2023-05-24 11:45:21 +0700

    pribadi
    ganti nama

---
+++ src/elaine.java
@@ -0,0 +1,10 @@
+public class elaine {
+    public static void main(String[] args) {
+        String nama = "Elaine";
+        int jumlahPerulangan = 5;
+
+        for (int i = 0; i < jumlahPerulangan; i++) {
+            System.out.println(nama);
+        }
+    }
+}
