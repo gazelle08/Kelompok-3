b44882fe01ec78695285911112316630a12cb9b3
YueJing76 <119746941+YueJing76@users.noreply.github.com>
2023-05-29 10:45:27 +0700

    fitur

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("1. Tampilkan Jadwal Tiket");
+        System.out.println("2. Melakukan Reservasi");
+        System.out.println("3. Membatalkan Reservasi");
+        System.out.println("4. Menampilkan Reservasi");
+        System.out.println("5. Keluar");
     }
 }
