938e25074591c44c8d25ac02e6325c49b262e09c
YueJing76 <119746941+YueJing76@users.noreply.github.com>
2023-05-29 14:16:06 +0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,9 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("1. Tampilkan Jadwal Konser");
         System.out.println("2. Melakukan Reservasi");
-        System.out.println("4. Membatalkan Reservasi");
+        System.out.println("3. Membatalkan Reservasi");
+        System.out.println("4. Pembayaran");
         System.out.println("5. Menampilkan Reservasi");
-        
-        System.out.println("7. Keluar");
+        System.out.println("6. Keluar");
     }
 }
