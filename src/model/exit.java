338fcbcd89a33d249b89f24ae781dfc9df0214c4
gazelle08 <134467035+gazelle08@users.noreply.github.com>
2023-05-30 21:18:54 +0700

    Update exit.java

--- src/model/exit.java
+++ src/model/exit.java
@@ -6,3 +6,11 @@ package model;
 public class exit {
     
 }
+
+/*
+ * Deskripsi:
+ * Tugas:
+ * Konfirmasi Keluar: Menampilkan pesan konfirmasi kepada pengguna untuk memastikan apakah mereka benar-benar ingin keluar dari aplikasi.
+ * Melakukan Log Out tugas ini akan melogout pengguna.
+ * Memberikan Pesan Terima Kasih: Menampilkan pesan terima kasih kepada pengguna atas penggunaan aplikasi dan memberikan informasi kontak atau dukungan jika diperlukan.
+ */
