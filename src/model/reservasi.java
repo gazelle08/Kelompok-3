e4ce57a2d933db95fbd31275b34ec723245f70e5
gazelle08 <134467035+gazelle08@users.noreply.github.com>
2023-05-30 20:49:48 +0700

    Update reservasi.java

--- src/model/reservasi.java
+++ src/model/reservasi.java
@@ -7,3 +7,20 @@ package model;
 public class reservasi {
     
 }
+/*
+ * Deskripsi : 
+ * Atribut :
+ * Daftar Konser: Daftar konser musik yang tersedia untuk reservasi tiket.
+ * Jenis Tiket: Pilihan jenis tiket yang ditawarkan untuk konser.
+ * Tanggal dan Waktu: Informasi tentang tanggal dan waktu konser yang tersedia.
+ * Harga: Harga tiket berdasarkan jenis tiket yang dipilih.
+ * Jumlah Tiket Tersedia: Jumlah tiket yang tersedia untuk setiap jenis tiket dan konser.
+ * Tempat Konser: Informasi lokasi atau tempat di mana konser akan diadakan.
+ * Tugas:
+ * Menampilkan Daftar Konser: Menampilkan daftar konser musik yang tersedia untuk reservasi tiket.
+ * Memilih Konser: Memilih konser musik yang ingin dihadiri oleh pengguna.
+ * Memilih Jenis Tiket: Memilih jenis tiket yang diinginkan, seperti tiket reguler atau tiket VIP.
+ * Memilih Tanggal dan Waktu: Memilih tanggal dan waktu konser yang tersedia.
+ * Memeriksa Ketersediaan Tiket: Memeriksa jumlah tiket yang tersedia untuk jenis tiket dan konser yang dipilih.
+ * Menghitung Harga: Menghitung harga total tiket berdasarkan jenis tiket dan jumlah tiket yang dipilih.
+ */
