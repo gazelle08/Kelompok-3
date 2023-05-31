6ed6464ff0e945deca9621f5a4ee849732c2b784
whiteplusyellow <elaine.agustina0501@gmail.com>
2023-05-31 08:13:14 +0700

    update pembayaran dan pembatalan

--- src/model/pembayaran.java
+++ src/model/pembayaran.java
@@ -1,24 +1,26 @@
 /*
  * Nama: Elaine Agustina
- * 
- * Deskripsi:
- *  Atribut:
- *      - Metode pembayaran 
- *      - Nomor kartu kredit 
- *      - Jumlah tiket
- *      - Jenis tiket
- *      - Harga
- *      - Total Pembayaran
- *  Tugas:
- *      Pengguna diberikan opsi untuk memilih metode pembayaran yang paling sesuai untuk mereka
- *      Nomor kartu kredit pengguna yang digunakan untuk melakukan pembayaran 
- *      Memperlihatkan kuantitas tiket yang telah dibeli pengguna
- *      Menampilkan jenis tiket yang dibeli oleh customer seperti reguler atau VIP
- *      Menampilkan harga tiket yang sudah dipilih sebelumnya
- *      Memperlihatkan nilai yang perlu dibayar oleh pengguna
  */     
 package model;
 
 public class pembayaran {
     
 }
+
+/*
+* Deskripsi:
+*  Atribut:
+*      - Metode pembayaran 
+*      - Nomor kartu kredit 
+*      - Jumlah tiket
+*      - Jenis tiket
+*      - Harga
+*      - Total Pembayaran
+*  Tugas:
+*      - Pengguna diberikan opsi untuk memilih metode pembayaran yang paling sesuai untuk mereka
+*      - Nomor kartu kredit pengguna yang digunakan untuk melakukan pembayaran 
+*      - Memperlihatkan kuantitas tiket yang telah dibeli pengguna
+*      - Menampilkan jenis tiket yang dibeli oleh customer seperti reguler atau VIP
+*      - Menampilkan harga tiket yang sudah dipilih sebelumnya
+*      - Memperlihatkan nilai yang perlu dibayar oleh pengguna
+*/
