0975f75f278829e98788c6f46a11078138b3f7b3
jerry <klik.jerryrobert@gmail.com>
2023-05-31 08:42:12 +0700

    update main

--- src/model/main.java
+++ src/model/main.java
@@ -2,7 +2,11 @@
  * Nama: Jerry Robert
  */
 package model;
-
+/*
+*Tugas:
+menampilkan pilihan menu yang ada dari no 1-6
+Customer akan diminta untuk input nomor yang ingin dipilih
+ */
 public class main {
     
 }
