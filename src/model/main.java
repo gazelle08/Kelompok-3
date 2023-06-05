26ef4f829f010ee69dd699e9221e9d98603970c3
YueJing76 <119746941+YueJing76@users.noreply.github.com>
2023-06-05 10:01:53 +0700

    update new

--- src/model/main.java
+++ src/model/main.java
@@ -8,5 +8,14 @@ menampilkan pilihan menu yang ada dari no 1-6
 Customer akan diminta untuk input nomor yang ingin dipilih
  */
 public class main {
-    
+    String pilihan ;
+    public String getPilihan() {
+        return this.pilihan;
+    }
+
+public void setPilihan(String pilihan) {
+        this.pilihan = pilihan;
+    }
 }
+
+
--- src/model/pembatalan.java
+++ src/model/pembatalan.java
@@ -4,9 +4,6 @@
 package model;
 
 public class pembatalan {
-    
-}
-
 /*
  * Deskripsi:
  *  Atribut:
@@ -16,3 +13,23 @@ public class pembatalan {
  *      - Pengguna memasukkan nomor referensi atau tiket yang ingin dibatalkan
  *      - Jumlah uang yang dikembalikan ke pengguna setelah pembatalan
  */
+
+int noReferensiTiket;
+double pengembalianUang;
+
+    public int getNoReferensiTiket() {
+        return this.noReferensiTiket;
+    }
+
+    public void setNoReferensiTiket(int noReferensiTiket) {
+        this.noReferensiTiket = noReferensiTiket;
+    }
+
+    public double getPengembalianUang() {
+        return this.pengembalianUang;
+    }
+
+    public void setPengembalianUang(double pengembalianUang) {
+        this.pengembalianUang = pengembalianUang;
+    }
+}
